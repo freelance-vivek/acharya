cf300ebee77b55801476e4a9d06463c63f6debac
vivek patel <vivek.patel@fabhotels.com>
2024-10-31 16:23:14 +0530

    remove footer and logo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import "./sass/main.scss";
 
 function App() {
   const aboutAcharyaRef = useRef(null);
+  const aboutAcharyaMissionRef = useRef(null);
+  const navCheckboxRef = useRef(null);
+
+  const handleNavigation = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+    if (navCheckboxRef.current) navCheckboxRef.current.checked = false; // Uncheck the navigation checkbox
+  };
   return (
     <div className="App">
      <nav className="navigation">
-            <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
+            <input type="checkbox" className="navigation__checkbox" id="navi-toggle"  ref={navCheckboxRef}/>
             <label htmlFor="navi-toggle" className="navigation__button">
                 <span className="navigation__icon">&nbsp;</span>
             </label>
@@ -15,11 +22,22 @@ function App() {
 
             <nav className="navigation__nav">
                 <ul className="navigation__list">
-                    <li className="navigation__item" onClick={() => aboutAcharyaRef.current.scrollIntoView({ behavior: 'smooth' })}><a  className="navigation__link"><span>01</span>About Acharya</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"><span>02</span>Your benfits</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"><span>03</span>Popular tours</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"><span>04</span>Stories</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"><span>05</span>Book now</a></li>
+                    <li className="navigation__item" ><a href= "#about-acharya" onClick={(e) => {
+                      e.preventDefault(); 
+                      handleNavigation(aboutAcharyaRef);
+                      aboutAcharyaRef.current.scrollIntoView({ behavior: 'smooth' })
+                    }
+                      
+                      } className="navigation__link"><span>01</span>About Acharya</a></li>
+                    
+                    <li className="navigation__item" ><a href= "#about-acharya" onClick={(e) => {
+                      e.preventDefault(); 
+                      handleNavigation(aboutAcharyaMissionRef);
+                      aboutAcharyaMissionRef.current.scrollIntoView({ behavior: 'smooth' })
+                    }
+                      
+                      } className="navigation__link"><span>02</span> Acharya Mission</a></li>
+                    
                     </ul>
             </nav>
         </nav>
@@ -29,7 +47,7 @@ function App() {
                     <span className="heading-primary--main">Acharya</span>
                     <span className="heading-primary--sub">Ashok Dwivedi</span>
                 </h1>
-                <span  onClick={() => aboutAcharyaRef.current.scrollIntoView({ behavior: 'smooth' })}>
+                <span  onClick={(e) => {e.preventDefault(); aboutAcharyaRef.current.scrollIntoView({ behavior: 'smooth' })}}>
                 <a className="btn btn--white btn--animated">Discover my work</a>
                 </span>
             </div>
@@ -94,7 +112,7 @@ In his youth, Acharya Ji moved to Nashik's Panchavati area in Maharashtra, where
                 </div>
             </section>
            
-            <section class="section-stories">
+            <section ref={aboutAcharyaMissionRef} className="section-stories" id="about-acharya-mission">
 
                 <div className="u-center-text u-margin-bottom-big">
                     <h2 className="heading-secondary">
@@ -122,114 +140,8 @@ Acharya Ashok Dwivedi Ji aims to empower yajamans with knowledge, freeing them f
                 </div>
                
             </section>
-
-            <section className="section-book">
-                <div className="row">
-                    <div className="book">
-                        <div className="book__form">
-                            <form action="#" className="form">
-                                <div className="u-margin-bottom-medium">
-                                    <h2 className="heading-secondary">
-                                        Start booking now
-                                    </h2>
-                                </div>
-
-                                <div className="form__group">
-                                    <input type="text" className="form__input" placeholder="Full name" id="name" required/>
-                                      <label for="name" className="form__label">Full name</label>
-                                </div>
-
-                                <div className="form__group">
-                                    <input type="email" className="form__input" placeholder="Email address" id="email" required/>
-                                    <label for="email" className="form__label">Email address</label>
-                                </div>
-
-                                <div className="form__group u-margin-bottom-medium">
-                                    <div className="form__radio-group">
-                                        <input type="radio" className="form__radio-input" id="small" name="size"/>
-                                        <label for="small" className="form__radio-label">
-                                            <span className="form__radio-button"></span>
-                                            Small tour group
-                                        </label>
-                                    </div>
-
-                                    <div className="form__radio-group">
-                                          <input type="radio" className="form__radio-input" id="large" name="size"/>
-                                        <label for="large" className="form__radio-label">
-                                            <span className="form__radio-button"></span>
-                                            Large tour group
-                                        </label>
-                                    </div>
-                                </div>
-
-                                <div className="form__group">
-                                    <button className="btn btn--green">Next step &rarr;</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            
         </main>
-        <footer className="footer">
-            <div className="footer__logo-box">
-
-                <picture className="footer__logo">
-                    <source srcset="img/logo-green-small-1x.png 1x, img/logo-green-small-2x.png 2x"
-                            media="(max-width: 37.5em)"/>
-                    <img srcset="img/logo-green-1x.png 1x, img/logo-green-2x.png 2x" alt="Full logo" src="img/logo-green-2x.png"/>
-                </picture>
-
-
-                
-            </div>
-            <div className="row">
-                <div className="col-1-of-2">
-                    <div className="footer__navigation">
-                        <ul className="footer__list">
-                            <li className="footer__item"><a href="#" className="footer__link">Company</a></li>
-                            <li className="footer__item"><a href="#" className="footer__link">Contact us</a></li>
-                            <li className="footer__item"><a href="#" className="footer__link">Carrers</a></li>
-                            <li className="footer__item"><a href="#" className="footer__link">Privacy policy</a></li>
-                            <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-1-of-2">
-                    <p className="footer__copyright">
-                        Built by <a href="#" className="footer__link">Jonas Schmedtmann</a> for his online course <a href="#" className="footer__link">Advanced CSS and Sass</a>.
-                        Copyright &copy; by Jonas Schmedtmann. You are 100% allowed to use this webpage for both personal
-                        and commercial use, but NOT to claim it as your own design. A credit to the original author, Jonas
-                        Schmedtmann, is of course highly appreciated!
-                    </p>
-                </div>
-            </div>
-        </footer>
-        <div className="popup" id="popup">
-            <div className="popup__content">
-                <div className="popup__left">
-                    <img src="img/nat-8.jpg" alt="Tour photo" className="popup__img"/>
-                    <img src="img/nat-9.jpg" alt="Tour photo" className="popup__img"/>
-                </div>
-                <div className="popup__right">
-                    <a href="#section-tours" className="popup__close">&times;</a>
-                    <h2 className="heading-secondary u-margin-bottom-small">Start booking now</h2>
-                    <h3 className="heading-tertiary u-margin-bottom-small">Important &ndash; Please read these terms before booking</h3>
-                    <p className="popup__text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Sed sed risus pretium quam. Aliquam sem et tortor consequat id. Volutpat odio facilisis mauris sit
-                        amet massa vitae. Mi bibendum neque egestas congue. Placerat orci nulla pellentesque dignissim enim
-                        sit. Vitae semper quis lectus nulla at volutpat diam ut venenatis. Malesuada pellentesque elit eget
-                        gravida cum sociis natoque penatibus et. Proin fermentum leo vel orci porta non pulvinar neque laoreet.
-                        Gravida neque convallis a cras semper. Molestie at elementum eu facilisis sed odio morbi quis. Faucibus
-                        vitae aliquet nec ullamcorper sit amet risus nullam eget. Nam libero justo laoreet sit. Amet massa
-                        vitae tortor condimentum lacinia quis vel eros donec. Sit amet facilisis magna etiam. Imperdiet sed
-                        euismod nisi porta.
-                    </p>
-                    <a href="#" className="btn btn--green">Book now</a>
-                </div>
-            </div>
-        </div>
     </div>
   );
 }
